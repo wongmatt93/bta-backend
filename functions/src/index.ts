c6da2aeb52907fb038aba50dd6f7659472f4b286
Matthew Wong <matthewwong@Matthews-MacBook-Air.local>
2022-08-25 17:21:14 -0400

    updated yelp api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,11 @@
 import * as functions from "firebase-functions";
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import express from "express";
+import cors from "cors";
+import yelpRouter from "./routes/yelpRouter";
+//import shoutOutRouter from './routes/shoutOutRouter';
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/yelp", yelpRouter);
+//app.use("/shoutouts", shoutOutRouter);
+export const api = functions.https.onRequest(app);
--- functions/src/routes/yelpRouter.ts
+++ functions/src/routes/yelpRouter.ts
@@ -1,16 +1,17 @@
 // require the express module
+import * as functions from "firebase-functions";
 import express from "express";
 import axios from "axios";
 // create a new Router object
 const yelpRouter = express.Router();
-// const key: string = functions.config().yelpKey
+const key: string = functions.config().yelp.key;
 yelpRouter.get("/", async (req, res) => {
   const { location } = req.query;
   const results = (
     await axios.get("https://api.yelp.com/v3/businesses/search", {
       params: { location },
       headers: {
-        Authorization: `Bearer`,
+        Authorization: `Bearer ${key}`,
       },
     })
   ).data;
