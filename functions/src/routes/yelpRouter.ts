8df8a2cb9149001bfeca7b57c8d92c2877483f8f
Damiquel Williams <damiquel@Damiquels-MacBook-Pro.local>
2022-08-30 17:06:13 -0400

    50 limit

--- functions/src/routes/yelpRouter.ts
+++ functions/src/routes/yelpRouter.ts
@@ -5,11 +5,24 @@ import axios from "axios";
 // create a new Router object
 const yelpRouter = express.Router();
 const key: string = functions.config().yelp.key;
-yelpRouter.get("/", async (req, res) => {
+yelpRouter.get("/restaurants", async (req, res) => {
   const { location } = req.query;
   const results = (
     await axios.get("https://api.yelp.com/v3/businesses/search", {
-      params: { location },
+      params: { location, limit: 50, categories: "restaurants" },
+      headers: {
+        Authorization: `Bearer ${key}`,
+      },
+    })
+  ).data;
+  res.status(200).json(results);
+});
+
+yelpRouter.get("/arts", async (req, res) => {
+  const { location } = req.query;
+  const results = (
+    await axios.get("https://api.yelp.com/v3/businesses/search", {
+      params: { location, limit: 50, categories: "arts" },
       headers: {
         Authorization: `Bearer ${key}`,
       },
